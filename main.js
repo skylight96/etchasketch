5867c7fb4b42066cebf0f3df33f284cbcb774d80
skylight96 <108493618+skylight96@users.noreply.github.com>
2022-12-13 22:26:19 -0800

    buttons added and centered (functional)

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let click = false;
+let currentColor = "black"
+let rgbMode = false
 
 document.addEventListener("DOMContentLoaded", function () {
     createBoard(16);
@@ -30,16 +32,48 @@ function createBoard(size) {
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let numDivs = size * size
-    
-        for (let i = 0; i < numDivs; i++) {
-            let div = document.createElement("div");
-            div.addEventListener("mouseover", function () {
-                if (click)
-                div.style.backgroundColor = "black"
-            })
-            board.insertAdjacentElement("beforeend", div);
-        }
+
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", function () {
+            if (click) {
+                console.log(div.style.backgroundColor)
+                if (div.style.backgroundColor == "") {
+                    div.style.backgroundColor = currentColor
+                    if (rgbMode) {
+                        div.style.backgroundColor = changeColor()
+                    }
+                }
+                else {
+                    let currentrgbArray = toRGBArray(div.style.backgroundColor)
+                    currentrgbArray[0] -= 10
+                    currentrgbArray[1] -= 10
+                    currentrgbArray[2] -= 10
+                    div.style.backgroundColor = "rgb(" + currentrgbArray[0] + "," + currentrgbArray[1] + ", " + currentrgbArray[2] + ")"
+                }
+            }
+        })
+        board.insertAdjacentElement("beforeend", div);
     }
+}
+
+function changeColor() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+// const toRGBArray = rgbStr => rgbStr.match(/\d+/g).map(Number);
+function toRGBArray(rgbStr) {
+    return rgbStr.match(/\d+/g).map(Number);
+}
+
+function toggleRGB() {
+    rgbMode = !rgbMode
+    document.getElementById("rgbButton").innerText = rgbMode ? "RGB" : "Black"
+}
 
 function getSize() {
     let input = prompt("What will the board size?");
@@ -57,6 +91,6 @@ function getSize() {
 }
 
 function resetBoard() {
-    let divs = document.querySelectorAll("div")
-    divs.forEach((div) => div.style.backgroundColor = "white")
+    let divs = document.getElementById("board").childNodes
+    divs.forEach((div) => div.style.backgroundColor = "")
 }
