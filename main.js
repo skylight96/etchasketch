97cb75864f69c63835f09777d0835716d9324a78
skylight96 <108493618+skylight96@users.noreply.github.com>
2022-12-13 19:59:25 -0800

    tailwind-intro steps

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 )
 
 function createBoard(size) {
-    let board = document.querySelector(".board");
+    let board = document.getElementById("board");
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
